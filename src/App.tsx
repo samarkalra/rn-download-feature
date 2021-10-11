fe621edf4ba76c90c68d6ce3af3c5301a79d4a98
samarkalra <skalra@ex2india.com>
2021-10-11 14:39:04 +0530

    Add image/pdf download functionality from base64

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import {Button, StyleSheet, View} from 'react-native';
+import {FileType} from './enums';
+import {downloadContent} from './file-download-utils';
+import {files} from './fileBase64';
 
 const App = () => {
+  const onDownloadImageBase64Press = () => {
+    downloadContent(files.imageBase64, FileType.Image);
+  };
+  const onDownloadPdfBase64Press = () => {
+    downloadContent(files.pdfBase64, FileType.Pdf);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.button}>
-        <Button title="download image from base64" onPress={() => {}} />
+        <Button
+          title="download image from base64"
+          onPress={onDownloadImageBase64Press}
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title="download pdf from base64"
+          onPress={onDownloadPdfBase64Press}
+        />
       </View>
     </View>
   );
---
+++ src/enums.ts
@@ -0,0 +1,16 @@
+export enum EncodingNames {
+  utf8 = 'utf8',
+  base64 = 'base64',
+  ascii = 'ascii',
+  uri = 'uri',
+}
+
+export enum FileType {
+  Image = 'image',
+  Pdf = 'pdf',
+}
+
+export enum FileExtensions {
+  jpg = '.jpg',
+  pdf = '.pdf',
+}
